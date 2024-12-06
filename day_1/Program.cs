342fac70858c506315ed5a615b3dae103df1fd48
MrDLC7 <mafia78ton@gmail.com>
2024-12-06 12:10:22 +0200

    Добавьте файлы проекта.

---
+++ day_1/Program.cs
@@ -0,0 +1,25 @@
+﻿public abstract class Vehicle
+{
+    //  Автоматисні властивості
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    //  Конструктор
+    protected Vehicle(string brand, string model, int year)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+
+    //  Віртуальний метод
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Трнспортний ззасіб: {Brand} {Model}, Рік випуску: {Year}");
+    }
+
+    //  Абстрактний метод
+    public abstract void StartEngine();
+}
+
